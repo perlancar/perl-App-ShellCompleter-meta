9b515becf2db50c52855b922aef1b24087e4c29f
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-11-04 12:48:13 +0700

    Initial commit

---
+++ lib/App/ShellCompleter/meta.pm
@@ -0,0 +1,16 @@
+package App::ShellCompleter::meta;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Shell completion for meta (Acme::MetaSyntactic CLI)
+
+=head1 SYNOPSIS
+
+See L<_meta> included in this distribution.
+
+
+=head1 SEE ALSO
+
+L<meta> from L<Acme::MetaSyntactic>
